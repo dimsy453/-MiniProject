76e392d95bc672597b0271cc75706822cd2f8693
dimsy453 <149811120+dimsy453@users.noreply.github.com>
2024-01-12 11:59:24 +0200

    Add files via upload

---
+++ Project.js
@@ -0,0 +1,51 @@
+// В index.html
+// 1 отримати масив об'єктів з endpoint`а https://jsonplaceholder.typicode.com/users
+// 2 Вивести id,name всіх user в index.html. Окремий блок для кожного user.
+// 3 Додати кожному блоку кнопку/посилання , при кліку на яку відбувається перехід  на сторінку user-details.html, котра має детальну інфорацію про об'єкт на який клікнули
+//
+//
+// На странице user-details.html:
+// 4 Вивести всю, без виключення, інформацію про об'єкт user на який клікнули
+// 5 Додати кнопку "post of current user", при кліку на яку, з'являються title всіх постів поточного юзера
+// (для получения постов используйте эндпоинт https://jsonplaceholder.typicode.com/users/USER_ID/posts)
+//     6 Каждому посту додати кнопку/посилання, при кліку на яку відбувається перехід на сторінку post-details.html, котра має детальну інфу про поточний пост.
+//
+//     На странице post-details.html:
+// 7 Вивести всю, без виключення, інформацію про об'єкт post на який клікнули .
+// 8 Нижчє інформаці про пост, вивести всі коментарі поточного поста (ендпоінт  - https://jsonplaceholder.typicode.com/posts/POST_ID/comments)
+//
+// Стилизація проєкта -
+// index.html - всі блоки з user - по 2 в рядок. кнопки/аосилвння розташувати під інформацією про user.
+//     user-details.html - блок з інфою про user зверху сторінки. Кнопка нижчє, на 90% ширини сторінки, по центру.
+//     блоки з короткою іфною про post - в ряд по 5 .
+//     post-details.html - блок з інфою про пост зверху. Коментарі - по 4 в ряд.
+//     Всі елементи котрі характеризують users, posts, comments візуалізувати, так, щоб було видно що це блоки (дати фон. марджини і тд)
+
+
+let url = new URL('https://jsonplaceholder.typicode.com/users');
+fetch(url)
+    .then(value => value.json())
+    .then(users => {
+        let div  = document.createElement('div');
+        for (const user of users) {
+            let div1 = document.createElement('div1');
+            let h2 = document.createElement('h2');
+            let h1 = document.createElement('h1');
+            let btn = document.createElement(`buton`)
+            console.log(user);
+            btn.innerText = ` DETAILS `;
+            h2.innerText = `${user.id}`;
+            h1.innerText = `${user.name}`;
+            div1.append(h2, h1, btn);
+            div.append(div1);
+
+            btn.onclick = function () {
+                location.href =  `./Project-detalis.html?user=` + JSON.stringify(user);
+            };
+        }
+        document.body.append(div);
+    });
+
+
+
+
